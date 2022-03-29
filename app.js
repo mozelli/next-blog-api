ac9ed82527a14061ddad4d32c8efcc864ab9fe08
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-03-29 12:46:54 -0300

    change route sistem

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ require('dotenv').config({ path: "./config.env" });
 const express = require('express');
 const app = express();
 
+/* ROUTES */
 app.get('/', (req, res) => {
     res.send("Running...");
 });
 
-app.use(require('./src/router'));
+const postsRouter = require('./src/routes/posts');
+app.use('/posts', postsRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Running on port ${ process.env.PORT }`);
